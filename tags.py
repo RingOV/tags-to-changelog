31be316d7aa42deebf4d1c9d1ea9d8f0727d2051
RingOV <ringov@mail.ru>
2014-01-12 21:26:26 +0300

    Update tags.py

--- tags.py
+++ tags.py
@@ -13,11 +13,20 @@ except:
     print 'не указана папка'
     exit()
 
+def get_tag_date(tag):
+    date = os.popen('cd '+DIR_NAME+' && git log -1 --format=%ai '+tag).readlines()
+    date = date[0].split()[0]
+    date = date.split('-')
+    date.reverse()
+    date = '.'.join(date)
+    return date
+
 tags = os.popen('cd '+DIR_NAME+' && git tag').readlines()
+
 for i in range(len(tags)-1):
-    print tags[i+1].rstrip('\n')
+    print tags[-i-1].rstrip('\n'), '(%s)'%get_tag_date(tags[-i-1])
     print '=========='
-    rel_note = os.popen('cd '+DIR_NAME+' && git log '+tags[i].strip()+'..'+tags[i+1].strip()+' --pretty=format:"- %s" --reverse').readlines()
+    rel_note = os.popen('cd '+DIR_NAME+' && git log '+tags[-i-2].strip()+'..'+tags[-i-1].strip()+' --pretty=format:" - %s" --reverse').readlines()
     for line in rel_note:
         print line.rstrip('\n')
     print ''
